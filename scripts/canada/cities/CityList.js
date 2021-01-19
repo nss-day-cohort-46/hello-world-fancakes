c75b19311b57ecdc6fff60e8fc0c90d65c92372c
Loganwebb1994 <loganwebb1994@gmail.com>
2021-01-19 14:26:51 -0600

    automated cities and celebrities.

--- scripts/canada/cities/CityList.js
+++ scripts/canada/cities/CityList.js
@@ -1,2 +1,21 @@
 import {useCity} from "/scripts/canada/cities/CityDataProvider.js"
-import {City} from "/scripts/canada/cities/"
+import {City} from "/scripts/canada/cities/City.js"
+
+export const cityList = () => {
+    const contentElement = document.querySelector(".contentContainer")
+    const allTheCities = useCity()
+    console.log(allTheCities,"logging all cities")
+    let cityHTMLRepresentations = ""
+    for (const cityObject of allTheCities){
+        cityHTMLRepresentations += City(cityObject) }
+       
+        contentElement.innerHTML += `
+    <section class="contentContainer__listOfCities">
+        <h2>Must-See Cities</h2>
+            <ul>  
+                ${cityHTMLRepresentations}
+            </ul>
+    </section>
+        `
+   
+}
