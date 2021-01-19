8b77c1f417e153282a1480734938b38d7d004830
hrlnsnchz <hrlnsnchz@gmail.com>
2021-01-19 15:01:14 -0600

    Added to the stuff I was working on previously in celebrities directory

--- scripts/japan/celebrities/CelebrityList.js
+++ scripts/japan/celebrities/CelebrityList.js
@@ -1,3 +1,12 @@
+import { useCelebrities } from "./CelebrityDataProvider"
+
 export const CelebrityList = () => {
-    
+    const contentElement = document.querySelector(".contentContainer")
+    const celebrities = useCelebrities()
+
+    contentElement.innerHTML += `
+    <article class ="celebList">
+        
+    </article>
+    `
 }
