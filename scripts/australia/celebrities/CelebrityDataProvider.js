94326cc9233b2b961594c916bb487f806efa93c6
kjk1325e <kaitlinjkelley@gmail.comm>
2021-01-19 11:31:06 -0600

    Added data for celebrities

---
+++ scripts/australia/celebrities/CelebrityDataProvider.js
@@ -0,0 +1,17 @@
+const celebrityData = [
+    {
+        name: "Steve Irwin"
+    },
+
+    {
+        name: "Rebel Wilson"
+    },
+
+    {
+        name: "Hugh Jackman"
+    }
+]
+
+export const useCelebrities = () => {
+    return celebrityData.slice()
+}
